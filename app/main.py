9e9cc454723baafd76f6b48aa017d25728340e14
Modi, Abhishek <Abhishek_Modi@homedepot.com>
2018-08-22 23:11:38 -0400

    Code adjusted

--- app/main.py
+++ app/main.py
@@ -24,27 +24,23 @@ airport_util = Airports()
 
 @app.route('/airportName', methods=['GET'])
 def airportName():
-  if request.endpoint == 'airportName':
-    """Given an airport IATA code, return that airport's name."""
-    iata_code = request.args.get('iataCode')
-    if iata_code is None:
-      return 'No IATA code provided.', 400
-    maybe_name = airport_util.get_airport_by_iata(iata_code)
-    if maybe_name is None:
-      return 'IATA code not found : %s' % iata_code, 400
-    return maybe_name, 200
-
-  elif request.endpoint == 'getMaxSiteVisitsDept':
-    visitstats_util = Visitstats()
-    visit_date = request.args.get('visitDate')
-    if visit_date is None:
-      return 'Provide a valid visit date',400
-    dept_name = get_max_sitvisits_dept(visit_date)
-    return dept_name
-  else: 
-    return "Hello Google Home"
+  """Given an airport IATA code, return that airport's name."""
+  iata_code = request.args.get('iataCode')
+  if iata_code is None:
+    return 'No IATA code provided.', 400
+  maybe_name = airport_util.get_airport_by_iata(iata_code)
+  if maybe_name is None:
+    return 'IATA code not found : %s' % iata_code, 400
+  return maybe_name, 200
+  
 @app.route('/getMaxSiteVisitsDept', methods=['GET'])
 def getMaxSiteVisitsDept():
-  return 'Test Success'
-  if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+  visitstats_util = Visitstats()
+  visit_date = request.args.get('visitDate')
+  if visit_date is None:
+    return 'Provide a valid visit date',400
+  dept_name = get_max_sitvisits_dept(visit_date)
+return dept_name
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=8080, debug=True)
